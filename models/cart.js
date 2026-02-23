6a8a45939f16a5e6c0d62d2eb1a6ea605870fd2a
Xmerlin7 <seifahmedjr7@gmail.com>
2026-02-23 12:54:33 +0200

    feat: add Mongoose models for Cart, Category, Product, RefreshToken, and User

---
+++ models/cart.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const cartSchema = new Schema(
+  {
+    user: {
+      ref: "User",
+      type: Schema.Types.ObjectId,
+      required: [true, "User is required"],
+    },
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          min: 1,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export default model("Cart", cartSchema);
---
+++ models/product.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+      unique: true,
+    },
+
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price Cannot be Negative"],
+    },
+
+    inStock: {
+      type: String,
+      enum: ["yes", "no"],
+      default: "yes",
+    },
+
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Category is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default model("Product", productSchema);
