16365d06c7be1ffd379597176bac53c6d4e9181e
Xmerlin7 <seifahmedjr7@gmail.com>
2026-02-24 19:58:51 +0200

    chore: remove unused pagination middleware

--- middlewares/pagination/pagination.middleware.js
+++
@@ -1,7 +0,0 @@
-const paginate = async (model)=>{
-    return(req, res,next) =>{
-        const page = req.query.page
-        const limit = req.query.page
-        const startIndex = (page - 1) * limit;
-    }
-}
