460e6ba397ccaaae3f4e62458e96e5045f9dcefb
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-10 17:20:42 +0200

    fix: refactor token handling in loginUser and logoutUser functions for clarity and consistency & Make The refresh token OneActiveDevice

--- services/account.service.js
+++ services/account.service.js
@@ -4,15 +4,13 @@ import User from "../models/user.js";
 import { create } from "./user.service.js";
 import ApiError from "../utils/ApiError.js";
 import RefreshTokenModel from "../models/tokens.js";
-import Tokens from "../models/tokens.js";
 
 export const getCurrentUser = async (req) => {
   const currentUserId = req.user.userId;
   const currentUser = await User.findById(currentUserId);
   console.log(currentUser);
-  return currentUser
+  return currentUser;
 };
-
 
 export const registerUser = async (data) => {
   const newUser = await create(data);
@@ -31,7 +29,7 @@ export const loginUser = async (data) => {
   const accessToken = jwt.sign(
     { userId: foundUser._id, role: foundUser.role },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: "15m" },
   );
   const refreshToken = jwt.sign(
     {
@@ -40,18 +38,19 @@ export const loginUser = async (data) => {
     process.env.REFRESH_TOKEN_SECRET,
     {
       expiresIn: "7d",
-    }
+    },
   );
   let hashedToken = await bcrypt.hash(refreshToken, 10);
   hashedToken = hashedToken.toString();
   await RefreshTokenModel.findOneAndUpdate(
-  { user: foundUser._id },        // 1. "Find" criteria
-  {                               // 2. "Update" data
-    token: hashedToken, 
-    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) 
-  },
-  { upsert: true, new: true }     // 3. Options
-);
+    { user: foundUser._id }, // 1. "Find" criteria
+    {
+      // 2. "Update" data
+      token: hashedToken,
+      expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    },
+    { upsert: true, new: true }, // 3. Options
+  );
   return { refreshToken, accessToken, foundUserName: foundUser.name };
 };
 export const logoutUser = async (req) => {
@@ -63,7 +62,7 @@ export const logoutUser = async (req) => {
     process.env.REFRESH_TOKEN_SECRET,
   );
   const { userId } = payload;
-  const allUserRefTokens = await Tokens.find({ user: userId });
+  const allUserRefTokens = await RefreshTokenModel.find({ user: userId });
 
   const comparedBooleanTokens = await Promise.all(
     allUserRefTokens.map((t) => bcrypt.compare(cookieRefreshToken, t.token)),
@@ -83,6 +82,10 @@ export const refreshUser = async (req) => {
     process.env.REFRESH_TOKEN_SECRET,
   );
   const { userId } = payload;
+  let currentToken = RefreshTokenModel.findById({ user: userId });
+  let isValid = bcrypt.compare(currentToken, cookieRefreshToken);
+  if (!isValid)
+    return new ApiError("U Are Only Allowed To login from one Device");
 
   const newAccessToken = jwt.sign(
     {
@@ -94,5 +97,6 @@ export const refreshUser = async (req) => {
       expiresIn: "15m",
     },
   );
+
   return newAccessToken;
 };
