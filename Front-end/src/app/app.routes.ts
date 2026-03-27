1ec1c601c64ae41b542ce3953379c2519c5517d1
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-27 12:41:14 +0200

    feat: Define application routes for login and redirect

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login.component'; // استدعاء الملف
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // طريق افتراضي عشان لو اليوزر دخل على "/" يوديه للـ login
+  { path: '', redirectTo: 'login', pathMatch: 'full' } 
+];
