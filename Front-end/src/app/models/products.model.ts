271fb33333247e2360c5587e3fa9b8a273bad002
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-27 12:42:18 +0200

    feat: Create Product model interface

---
+++ Front-end/src/app/models/products.model.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  stock: number;
+  rating?: {
+    rate: number;
+    count: number;
+  };
+}
