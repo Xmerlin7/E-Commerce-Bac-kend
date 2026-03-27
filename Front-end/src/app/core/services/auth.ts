8cd2ad84768200148c4d0b72caac0b74fdb8ec4b
Xmerlin7 <seifahmedjr7@gmail.com>
2026-03-27 14:39:50 +0200

    refactor: Clean up AuthService code and improve URL handling

--- Front-end/src/app/core/services/auth.ts
+++ Front-end/src/app/core/services/auth.ts
@@ -5,28 +5,25 @@ import { AuthResponse, User } from '../../models/users.model';
 import { tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private http = inject(HttpClient); // الـ Injectable اللي كنت ناسيها، أهي جت دليفري!
-  private apiUrl = `${environment.apiUrl}/auth`; // ده الـ URL اللي ربطناه بالباك-إيند
+  private http = inject(HttpClient);
+  private url = `${environment.apiUrl}`;
 
-  // الـ Signal ده هو "المخزن" اللي هيعرفنا اليوزر عامل Login ولا لأ في أي مكان في الموقع
   currentUser = signal<User | null>(null);
 
   login(credentials: any) {
-  // تأكد من الـ URL هنا يا سيف 👈
-  return this.http.post<AuthResponse>(`http://localhost:8080/api/login`, credentials).pipe(
-    tap((res: AuthResponse) => {
-  if (res.token && res.user) {
-    localStorage.setItem('userToken', res.token);
-    
-    // بنستخدم "as User" عشان نقول للـ TS: "ثق فيا أنا عارف إن الـ role صح"
-    this.currentUser.set(res.user as User); 
-    
-    console.log('Login Success! ✅ User Role:', res.user.role);
+    return this.http.post<AuthResponse>(`${this.url}/login`, credentials).pipe(
+      tap((res: AuthResponse) => {
+        if (res.token && res.user) {
+          localStorage.setItem('userToken', res.token);
+
+          this.currentUser.set(res.user as User);
+
+          console.log('Login Success! ✅ User Role:', res.user.role);
+        }
+      }),
+    );
   }
-})
-  );
-}
 }
